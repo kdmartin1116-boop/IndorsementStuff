5234a67ac86fb5267ad0a6b7ea953bb3acd075ee
MrNAM205 <ejgotem1@gmail.com>
2025-10-07 20:16:56 -0500

    update

--- verobrix/agents/emotion_reflector.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Execute the Python emotional reflector script, passing all arguments to it.
-python3 "$(dirname "$0")/emotional_reflector.py" "$@"
--- verobrix/agents/nlp_parser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Execute the Python NLP parser script, passing all arguments to it.
-python3 /home/mrnam205/Desktop/try2/agents/nlp_parser.py "$@"
